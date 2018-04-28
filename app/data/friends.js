d2255ff16d9a107308f2890fa4b3245e4d3b906e
MattSkrajner <matthew.skrajner@gmail.com>
2018-04-28 01:37:11 -0400

    commit

---
+++ app/data/friends.js
@@ -0,0 +1,142 @@
+// You should save your application's data inside of app/data/friends.js as an array of objects. 
+ 
+var friends = [
+
+{
+name: "Ahmed",
+photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}, 
+
+{
+    name: "Joe",
+    photo: "http://www.joepulizzi.com/wp-content/uploads/2013/12/JoePulizzi-300x452.jpg",
+    scores: [
+          2,
+          5,
+          3,
+          5,
+          2,
+          3,
+          2,
+          1,
+          1,
+          1
+        ]
+},
+
+{
+    name: "Katie",
+    photo: "http://assets.pewresearch.org/wp-content/uploads/sites/12/2014/10/08145133/Simmons_Katie.jpg",
+    scores: [
+          5,
+          5,
+          5,
+          5,
+          5,
+          5,
+          3,
+          3,
+          3,
+          2
+        ]
+}, 
+
+{
+    name: "Frank",
+    photo: "https://www.allenmorecounseling.com/wp-content/uploads/2011/07/Philip-Frank-Website-Photo1.jpg",
+    scores: [
+          2,
+          2,
+          3,
+          4,
+          5,
+          5,
+          2,
+          5,
+          1,
+          1
+        ]
+}, 
+
+{
+    name: "Megan",
+    photo: "https://static.wixstatic.com/media/84770f_e48cf4fbffb865eb101de1010785b42d.jpg/v1/fill/w_885,h_479,al_c,lg_1,q_85/84770f_e48cf4fbffb865eb101de1010785b42d.webp",
+    scores: [
+          1,
+          1,
+          1,
+          1,
+          1,
+          2,
+          2,
+          4,
+          4,
+          1
+        ]
+}, 
+
+{
+    name: "Olivia",
+    photo: "https://orangephotography.com/wp-content/gallery/svb/OliviaKlei_Headshot_High_02.23.16.jpg",
+    scores: [
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          5,
+          1,
+          1
+        ]
+}, 
+
+{
+    name: "Jerry",
+    photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0jMIU0ylVzdj1o0C56xjw3QqNMKcbBX8CH0MDnSH1XwMxIxU2",
+    scores: [
+          5,
+          5,
+          4,
+          4,
+          5,
+          3,
+          4,
+          5,
+          4,
+          3
+        ]
+}, 
+
+{
+    name: "David",
+    photo: "https://c8.alamy.com/comp/C5T5AA/david-hasselhoff-head-shot-2011-image-copyright-hollywood-headshots-C5T5AA.jpg",
+    scores: [
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2,
+          2
+        ]
+}, 
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,29 @@
+// Your server.js file should require the basic npm packages we've used in class: express, body-parser and path.
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express(); 
+
+var PORT = process.env.PORT || 8080
+
+// insert code from npm website
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
+
+// parse various different custom JSON types as JSON
+app.use(bodyParser.json({ type: 'application/*+json' }))
+ 
+// parse some custom thing into a Buffer
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
+ 
+// parse an HTML body into a string
+app.use(bodyParser.text({ type: 'text/html' }))
+
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
